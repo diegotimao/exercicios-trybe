572e78a560ba165fbd0238d994ff3b507a2c07f3
Diego Cavalcanti <diegotimao1104@gmail.com>
2022-01-24 11:38:47 -0300

    Envio de exercicio Bash e Shell

--- fundamentos/bloco-01-unix-e-shell/exercicio02.js
+++ fundamentos/bloco-01-unix-e-shell/exercicio02.js
@@ -1,4 +1,4 @@
-// 1. Na pasta `unix_tests` , baixe um arquivo com os nomes de todos os países do mundo utilizando o comando [curl:](https://linux.die.net/man/1/curl)
+// Na pasta `unix_tests` , baixe um arquivo com os nomes de todos os países do mundo utilizando o comando [curl:](https://linux.die.net/man/1/curl)
 `curl -o countries.txt "https://gist.githubusercontent.com/kalinchernev/486393efcca01623b18d/raw/daa24c9fea66afb7d68f8d69f0c4b8eeb9406e83/countries"`
 
 ```bash

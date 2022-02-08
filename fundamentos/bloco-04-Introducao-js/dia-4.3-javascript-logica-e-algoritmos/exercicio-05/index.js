c65d77954445982e339aab4529348cdabc607c35
Diego Cavalcanti <diegotimao1104@gmail.com>
2022-02-08 14:05:02 -0300

    Envio de exercicios dia 4.4 Objetos e Funçoes

---
+++ fundamentos/bloco-04-Introducao-js/dia-4.3-javascript-logica-e-algoritmos/exercicio-05/index.js
@@ -0,0 +1,11 @@
+let valor = 15;
+let simbolo = '*';
+let linha = '';
+
+for(let lineIndex = 1; lineIndex <= valor; lineIndex += 1){    
+    linha = linha + simbolo;
+};
+
+for(let lineIndex = 0; lineIndex < 5; lineIndex += 1){
+    console.log(linha);
+};
